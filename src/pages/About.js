486ee4d5a86172b63371f4791e756337e01b1a6e
Keenan Thompson <keenan@keenant.com>
2018-04-04 01:35:07 -0500

    Link to Github org

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Button, Container, Divider, Icon} from "semantic-ui-react";
 
-const githubLink = "https://github.com/thekeenant/madgrades";
+const githubLink = "https://github.com/Madgrades";
 const gradeDistLink = "https://registrar.wisc.edu/wp-content/uploads/sites/36/2017/09/report-gradedistribution-2010-2011spring.pdf";
 
 const About = () => (

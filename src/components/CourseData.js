25f1f0730c4ce5e920f090044139bac3e910d65a
Keenan Thompson <keenan@keenant.com>
2018-04-10 14:32:38 -0500

    meta tags

---
+++ src/components/CourseData.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import utils from '../utils';
+
+class CourseData extends Component {
+  static propTypes = {
+    uuid: PropTypes.string.isRequired,
+    onDataLoad: PropTypes.func.isRequired
+  };
+
+  componentWillMount = () => {
+    const { actions, uuid, data, onDataLoad } = this.props;
+    actions.fetchCourse(uuid);
+
+    if (data && !data.isFetching) {
+      onDataLoad(data);
+    }
+  };
+
+  componentDidUpdate = this.componentWillMount;
+
+  render = () => null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const { uuid } = ownProps;
+
+  const { courses } = state;
+  const data = courses.data[uuid];
+
+  return {
+    data
+  }
+}
+
+
+export default connect(mapStateToProps, utils.mapDispatchToProps)(CourseData)
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -4,6 +4,7 @@ import {Container, Divider, Header} from 'semantic-ui-react';
 import CourseChartViewer from '../components/CourseChartViewer';
 import CourseGpaChart from '../components/CourseGpaChart';
 import {parse, stringify} from 'qs';
+import CourseData from "../components/CourseData";
 
 const Course = ({ match, location, history }) => {
   document.title = ' - Madgrades';
@@ -20,12 +21,24 @@ const Course = ({ match, location, history }) => {
     history.push(`/courses/${uuid}?${stringify(params)}`)
   };
 
-  const onNameLoad = (name) => {
-    document.title = name + ' UW Madison Grade Distribution - Madgrades';
+  const onCourseDataLoad = (data) => {
+    const { name, subjects, number } = data;
+
+    let visibleName = name || 'Unknown Name';
+    let title = visibleName + ' - Madgrades';
+
+    let desc = subjects.map((s) => s.abbreviation).slice(0, 3).join(', ') + ' ' + number;
+    desc += ' UW Madison course grade distribution and average GPA over time or by instructor.';
+
+    document.title = title;
+    document.querySelector('meta[name="description"]').setAttribute('content', desc);
   };
 
   return (
       <Container className='Course'>
+        <CourseData
+          uuid={uuid}
+          onDataLoad={onCourseDataLoad}/>
         <Header size='huge'>
           <Header.Content style={{maxWidth: '100%'}}>
             <CourseName
@@ -34,8 +47,7 @@ const Course = ({ match, location, history }) => {
             <Header.Subheader style={{maxWidth: '100%'}}>
               <CourseName
                   uuid={uuid}
-                  asSubjectAndNumber={true}
-                  onNameLoad={onNameLoad}/>
+                  asSubjectAndNumber={true}/>
             </Header.Subheader>
           </Header.Content>
         </Header>

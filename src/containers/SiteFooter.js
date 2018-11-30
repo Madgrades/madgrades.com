c4d6a377fbbea2398d78d7ce5cdcd15269548e92
Keenan Thompson <keenan@keenant.com>
2018-11-30 00:54:01 -0600

    Add uptime/status to footer (#11)
    * Update npm packages
    
    npm audit fixes
    
    * Improved footer, added status pill/label
    
    * Add icons to footer pills

--- src/containers/SiteFooter.js
+++ src/containers/SiteFooter.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import {Container, Divider, List} from 'semantic-ui-react';
+import {Container, Divider, List, Label, Icon} from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
 import logo from '../assets/logo-black.svg'
 import {Col, Row} from 'react-flexbox-grid';
 import gitRevFile from '../assets/git-rev.txt';
 import LatestTerm from '../components/LatestTerm';
+import ApiStatusPill from './ApiStatusPill';
 
 const commitUrl = 'https://github.com/Madgrades/madgrades.com/commit/';
 
@@ -27,52 +28,39 @@ class SiteFooter extends Component {
       <div className='SiteFooter'>
         <Divider/>
         <Container>
-          <Row middle='xs'>
-            <Col style={{display: 'flex'}}>
+          <Row between='xs'>
+            <Col sm style={{display: 'flex'}}>
               <Link to='/'>
                 <img alt='Madgrades Logo' className='logo' src={logo} style={{verticalAlign: 'middle'}}/>
               </Link>
               <span style={{marginLeft: '10px'}}>
-                Made by {' '}
-                <a
-                  href='https://keenant.com'
-                  target='_blank'
-                  rel='noopener noreferrer'>
-                  Keenan Thompson
-                </a>
+                <List horizontal floated='right'>
+                  <List.Item>
+                    Made by {' '}
+                    <a
+                      href='https://keenant.com'
+                      target='_blank'
+                      rel='noopener noreferrer'>
+                      Keenan Thompson
+                    </a>
+                    <br/>
+                    <small>
+                      Madgrades is unaffiliated with UW Madison. Courses updated <LatestTerm/>.
+                    </small>
+                  </List.Item>
+                </List>
                 <br/>
-                <small>
-                  Madgrades is unaffiliated with UW Madison. All data is provided by the {' '}
-                  <a
-                    href='https://registrar.wisc.edu/grade-reports/'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    registrar office
-                  </a>.
-                </small>
               </span>
             </Col>
-            <Col xs>
-              <List horizontal floated='right'>
-                <List.Item>
-                  Latest: <LatestTerm/>
-                </List.Item>
-                <List.Item>
-                  <a
-                    href={process.env.REACT_APP_MADGRADES_API}
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    API
-                  </a>
-                </List.Item>
-                <List.Item>
-                  <a href={`${commitUrl}${this.state.gitRev}`}
-                     target='_blank'
-                     rel='noopener noreferrer'>
-                    Rev: {this.state.gitRev || 'Source'}
-                  </a><br/>
-                </List.Item>
-              </List>
+            <Col auto='xs'>
+              <Label color='teal' horizontal as='a' href={process.env.REACT_APP_MADGRADES_API}>
+                <Icon name='code'/>
+                API
+              </Label>
+              <Label color='black' horizontal as='a' href={`${commitUrl}${this.state.gitRev}`}>
+                <Icon name='code branch'/> rev {this.state.gitRev || 'Source'}
+              </Label>
+              <ApiStatusPill/>
             </Col>
           </Row>
         </Container>

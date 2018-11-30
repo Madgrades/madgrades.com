c4745b6118d89e22b79b08b56263879cacc5c557
Keenan Thompson <keenan@keenant.com>
2018-11-30 00:59:32 -0600

    Allow custom API key for uptime robot
    clients should be able to access it anyways

--- src/utils/fetchStatus.js
+++ src/utils/fetchStatus.js
@@ -1,6 +1,6 @@
 
 const API_URL = 'https://api.uptimerobot.com/v2/getMonitors';
-const apiKey = 'm781497538-14313d0dceb833046f430ad9';
+const API_KEY = process.env.REACT_APP_UPTIME_ROBOT_API_KEY;
 
 const errorCodes = {
   0: "PAUSED",
@@ -10,28 +10,32 @@ const errorCodes = {
   9: "DOWN"
 }
 
+
 async function fetchStatus() {
   const now = new Date();
   const before = new Date(); 
   before.setDate(now.getDate() - 7);
+  
+  
 
   return await fetch(API_URL, {
     method: 'post',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: `api_key=${apiKey}&format=json&custom_uptime_ranges=${before/1E3|0}_${now/1E3|0}`
+    body: `api_key=${API_KEY}&format=json&custom_uptime_ranges=${before/1E3|0}_${now/1E3|0}`
   })
     .then(async res => {
       const json = await res.json();
-      return json["monitors"][0];
-    })
-    .then(monitor => {
+      if (!json || !json["monitors"]) {
+        return undefined;
+      }
+      const monitor = json["monitors"][0];
       return {
         "uptime": parseFloat(monitor["custom_uptime_ranges"], 100),
         "status": errorCodes[monitor["status"]]
       }
-    });
+    })
 }
 
 export default fetchStatus;

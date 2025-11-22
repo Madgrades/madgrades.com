7ef230f9e21cb07523f73852c498dedcd08b2e37
Keenan Thompson <keenan@keenant.com>
2025-11-22 23:03:45 +0000

    Convert CourseComparison and CourseChartViewer
    - 20/24 class components converted (83%)
    - Lint errors: 723 → 721
    - 4 class components remaining (excluding template)
    - Used useRef for chart export functionality

--- src/components/CourseChartViewer.tsx
+++ src/components/CourseChartViewer.tsx
@@ -1,227 +1,176 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { useState, useEffect, useRef } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
 import utils from '../utils';
 import { RootState } from '../types';
-import { Button, Dropdown, Form } from "semantic-ui-react";
-import { Row, Col } from "./Grid";
-import TermSelect from "../containers/TermSelect";
-import CourseChart from "./CourseChart";
-import domtoimage from "dom-to-image";
-import FileSaver from "file-saver";
+import { Button, Dropdown, Form } from 'semantic-ui-react';
+import { Row, Col } from './Grid';
+import TermSelect from '../containers/TermSelect';
+import CourseChart from './CourseChart';
+import domtoimage from 'dom-to-image';
+import FileSaver from 'file-saver';
 
-interface CourseChartViewerProps {
+interface OwnProps {
   uuid: string;
   termCode?: number;
   instructorId?: number;
   onChange?: (params: { termCode?: number; instructorId?: number }) => void;
-  data?: any;
-  course?: any;
-  actions?: any;
 }
 
-interface CourseChartViewerState {
-  isExporting: boolean;
-}
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type Props = OwnProps & PropsFromRedux;
 
-class CourseChartViewer extends Component<CourseChartViewerProps, CourseChartViewerState> {
-  state: CourseChartViewerState = {
-    isExporting: false,
-  };
+function CourseChartViewer({
+  uuid,
+  termCode,
+  instructorId,
+  onChange = () => {},
+  data,
+  actions,
+}: Props) {
+  const [isExporting, setIsExporting] = useState(false);
+  const chartRef = useRef<HTMLDivElement>(null);
 
-  static defaultProps = {
-    onChange: ({ termCode, instructorId }: { termCode?: number; instructorId?: number }) => {},
-  };
-
-  fetchCourseGrades = () => {
-    const { uuid, actions } = this.props;
+  useEffect(() => {
     actions.fetchCourseGrades(uuid);
+  }, [uuid, actions]);
+
+  const onTermCodeChange = (newTermCode: number) => {
+    onChange({ termCode: newTermCode, instructorId });
   };
 
-  componentDidMount = this.fetchCourseGrades;
-
-  componentDidUpdate = (prevProps: CourseChartViewerProps) => {
-    if (prevProps.uuid !== this.props.uuid) {
-      this.fetchCourseGrades();
-    }
+  const onInstructorChange = (event: any, { value }: { value: number }) => {
+    onChange({ termCode, instructorId: value });
   };
 
-  onTermCodeChange = (termCode) => {
-    const { onChange, instructorId } = this.props;
+  const onSaveChart = () => {
+    if (isExporting || !chartRef.current) return;
 
-    this.setState(
-      {
-        termCode,
-      },
-      () => {
-        onChange({ termCode, instructorId });
-      }
-    );
-  };
-
-  onInstructorChange = (event, { value }) => {
-    const { onChange, termCode } = this.props;
-
-    this.setState(
-      {
-        instructorId: value,
-      },
-      () => {
-        onChange({ termCode, instructorId: value });
-      }
-    );
-  };
-
-  onSaveChart = () => {
-    if (this.state.isExporting) return;
-
-    this.setState({
-      isExporting: true,
-    });
+    setIsExporting(true);
 
     domtoimage
-      .toBlob(this.chart, { bgcolor: "#fff" })
+      .toBlob(chartRef.current, { bgcolor: '#fff' })
       .then((blob) => {
         FileSaver.saveAs(blob, `madgrades-${new Date().toISOString()}.png`);
-        this.setState({
-          isExporting: false,
-        });
+        setIsExporting(false);
       })
       .catch((error) => {
-        this.setState({
-          isExporting: false,
-        });
+        setIsExporting(false);
       });
   };
 
-  render = () => {
-    const { uuid, data, instructorId, termCode } = this.props;
-    const { isExporting } = this.state;
+  let instructorOptions: any[] = [],
+    termCodes: number[] = [],
+    termDescs: { [key: number]: string } = {},
+    instructorText = 'All instructors',
+    termText = 'All semesters';
 
-    let instructorOptions = [],
-      termCodes = [],
-      termDescs = {},
-      instructorText = "All instructors",
-      termText = "All semesters";
+  if (data && !data.isFetching) {
+    instructorOptions.push({
+      key: 0,
+      value: 0,
+      text: instructorText,
+    });
+    instructorOptions = instructorOptions.concat(
+      data.instructors.map((i: any) => {
+        return {
+          key: i.id,
+          value: i.id,
+          text: i.name,
+          description: utils.grades.gpa(i.cumulative, true),
+        };
+      })
+    );
 
-    if (data && !data.isFetching) {
-      instructorOptions.push({
-        key: 0,
-        value: 0,
-        text: instructorText,
+    data.courseOfferings.forEach((o: any) => {
+      termCodes.push(o.termCode);
+      termDescs[o.termCode] = utils.grades.gpa(o.cumulative, true);
+    });
+
+    // if instructor selected, filter term codes
+    if (instructorId) {
+      let instructorName = 'N/A';
+
+      termCodes = termCodes.filter((code) => {
+        if (code === 0) return true;
+
+        const instructor = data.instructors.filter((i: any) => i.id === instructorId)[0];
+
+        if (!instructor) return true;
+
+        instructorName = instructor.name;
+        return instructor.terms.map((term: any) => term.termCode).includes(code);
       });
-      instructorOptions = instructorOptions.concat(
-        data.instructors.map((i) => {
-          return {
-            key: i.id,
-            value: i.id,
-            text: i.name,
-            description: utils.grades.gpa(i.cumulative, true),
-          };
-        })
-      );
 
-      data.courseOfferings.forEach((o) => {
-        termCodes.push(o.termCode);
-        termDescs[o.termCode] = utils.grades.gpa(o.cumulative, true);
-      });
-
-      // if instructor selected, filter term codes
-      if (instructorId) {
-        let instructorName = "N/A";
-
-        termCodes = termCodes.filter((code) => {
-          if (code === 0) return true;
-
-          const instructor = data.instructors.filter(
-            (i) => i.id === instructorId
-          )[0];
-
-          if (!instructor) return true;
-
-          instructorName = instructor.name;
-          return instructor.terms.map((term) => term.termCode).includes(code);
-        });
-
-        termText += ` (${instructorName})`;
-      }
-
-      // if term code selected, filter instructor options
-      if (termCode) {
-        const termName = utils.termCodes.toName(termCode);
-        instructorText += ` (${termName})`;
-
-        instructorOptions = instructorOptions.filter((option) => {
-          const id = option.value;
-
-          if (id === 0) return true;
-
-          const instructor = data.instructors.filter((i) => i.id === id)[0];
-          return instructor.terms
-            .map((term) => term.termCode)
-            .includes(termCode);
-        });
-      }
-
-      instructorOptions[0].text = instructorText;
+      termText += ` (${instructorName})`;
     }
 
-    const instructorChosen = instructorId || undefined,
-      termChosen = termCode || undefined;
+    // if term code selected, filter instructor options
+    if (termCode) {
+      const termName = utils.termCodes.toName(termCode);
+      instructorText += ` (${termName})`;
 
-    return (
-      <Row>
-        <Col xs={12} md={12} lg={4}>
-          <Form style={{ marginBottom: "1em" }}>
-            <Form.Field>
-              <label>Instructors</label>
-              <Dropdown
-                fluid
-                selection
-                search
-                options={instructorOptions}
-                onChange={this.onInstructorChange}
-                value={instructorId}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Semesters</label>
-              <TermSelect
-                value={termCode}
-                termCodes={termCodes}
-                includeCumulative={true}
-                cumulativeText={termText}
-                onChange={this.onTermCodeChange}
-                descriptions={termDescs}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Export</label>
-              <Button
-                icon="download"
-                loading={isExporting}
-                basic
-                size="small"
-                content="Save Image"
-                onClick={this.onSaveChart}
-              />
-            </Form.Field>
-          </Form>
-        </Col>
-        <Col xs={12} lg={8}>
-          <div
-            ref={(ref) => (this.chart = ref)}
-            style={{ marginBottom: "1em" }}
-          >
-            <CourseChart
-              uuid={uuid}
-              instructorId={instructorChosen}
-              termCode={termChosen}
+      instructorOptions = instructorOptions.filter((option) => {
+        const id = option.value;
+
+        if (id === 0) return true;
+
+        const instructor = data.instructors.filter((i: any) => i.id === id)[0];
+        return instructor.terms.map((term: any) => term.termCode).includes(termCode);
+      });
+    }
+
+    instructorOptions[0].text = instructorText;
+  }
+
+  const instructorChosen = instructorId || undefined,
+    termChosen = termCode || undefined;
+
+  return (
+    <Row>
+      <Col xs={12} md={12} lg={4}>
+        <Form style={{ marginBottom: '1em' }}>
+          <Form.Field>
+            <label>Instructors</label>
+            <Dropdown
+              fluid
+              selection
+              search
+              options={instructorOptions}
+              onChange={onInstructorChange}
+              value={instructorId}
             />
-          </div>
-        </Col>
-      </Row>
-    );
-  };
+          </Form.Field>
+          <Form.Field>
+            <label>Semesters</label>
+            <TermSelect
+              value={termCode}
+              termCodes={termCodes}
+              includeCumulative={true}
+              cumulativeText={termText}
+              onChange={onTermCodeChange}
+              descriptions={termDescs}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Export</label>
+            <Button
+              icon="download"
+              loading={isExporting}
+              basic
+              size="small"
+              content="Save Image"
+              onClick={onSaveChart}
+            />
+          </Form.Field>
+        </Form>
+      </Col>
+      <Col xs={12} lg={8}>
+        <div ref={chartRef} style={{ marginBottom: '1em' }}>
+          <CourseChart uuid={uuid} instructorId={instructorChosen} termCode={termChosen} />
+        </div>
+      </Col>
+    </Row>
+  );
 }
 
 function mapStateToProps(state: RootState, ownProps: { uuid: string }) {
--- src/components/CourseComparison.tsx
+++ src/components/CourseComparison.tsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import { Container, Grid, Segment, Header, Button } from 'semantic-ui-react';
 import CourseName from './CourseName';
 import CourseChartViewer from './CourseChartViewer';
@@ -13,150 +12,127 @@ interface CourseComparisonProps {
   navigate: (path: string) => void;
 }
 
-class CourseComparison extends Component<CourseComparisonProps> {
-
-  handleCourse1Change = (params: { instructorId?: number; termCode?: number }) => {
-    const { course1Uuid, course2Uuid, location, navigate } = this.props;
+function CourseComparison({
+  course1Uuid,
+  course2Uuid,
+  onRemoveComparison,
+  location,
+  navigate,
+}: CourseComparisonProps) {
+  const handleCourse1Change = (params: { instructorId?: number; termCode?: number }) => {
     const currentParams = parse(location.search.substr(1));
 
     const newParams = {
-      instructorId: params.instructorId || "0",
-      termCode: params.termCode || "0",
-      course2InstructorId: currentParams.course2InstructorId || "0",
-      course2TermCode: currentParams.course2TermCode || "0",
+      instructorId: params.instructorId || '0',
+      termCode: params.termCode || '0',
+      course2InstructorId: currentParams.course2InstructorId || '0',
+      course2TermCode: currentParams.course2TermCode || '0',
       compareWith: course2Uuid,
     };
     navigate(`/courses/${course1Uuid}?${stringify(newParams)}`);
   };
 
-  handleCourse2Change = (params: { instructorId?: number; termCode?: number }) => {
-    const { course1Uuid, course2Uuid, location, navigate } = this.props;
+  const handleCourse2Change = (params: { instructorId?: number; termCode?: number }) => {
     const currentParams = parse(location.search.substr(1));
 
     const newParams = {
-      instructorId: currentParams.instructorId || "0",
-      termCode: currentParams.termCode || "0",
-      course2InstructorId: params.instructorId || "0",
-      course2TermCode: params.termCode || "0",
+      instructorId: currentParams.instructorId || '0',
+      termCode: currentParams.termCode || '0',
+      course2InstructorId: params.instructorId || '0',
+      course2TermCode: params.termCode || '0',
       compareWith: course2Uuid,
     };
     navigate(`/courses/${course1Uuid}?${stringify(newParams)}`);
   };
 
-  handleReplaceCourse1 = () => {
-    const { course2Uuid, navigate } = this.props;
-    // Navigate to search with course2 as the comparison target
+  const handleReplaceCourse1 = () => {
     navigate(`/search?compareWith=${course2Uuid}&replacing=1`);
   };
 
-  handleReplaceCourse2 = () => {
-    const { course1Uuid, navigate } = this.props;
-    // Navigate to search with course1 as the comparison target
+  const handleReplaceCourse2 = () => {
     navigate(`/search?compareWith=${course1Uuid}&replacing=2`);
   };
 
-  render() {
-    const { course1Uuid, course2Uuid, onRemoveComparison, location } =
-      this.props;
-    const params = parse(location.search.substr(1));
+  const params = parse(location.search.substr(1));
 
-    const course1InstructorId = parseInt(params.instructorId || "0", 10);
-    const course1TermCode = parseInt(params.termCode || "0", 10);
-    const course2InstructorId = parseInt(params.course2InstructorId || "0", 10);
-    const course2TermCode = parseInt(params.course2TermCode || "0", 10);
+  const course1InstructorId = parseInt(params.instructorId || '0', 10);
+  const course1TermCode = parseInt(params.termCode || '0', 10);
+  const course2InstructorId = parseInt(params.course2InstructorId || '0', 10);
+  const course2TermCode = parseInt(params.course2TermCode || '0', 10);
 
-    return (
-      <Container fluid style={{ padding: "20px 40px" }}>
-        <div style={{ display: "flex", gap: "1em", marginBottom: "2em" }}>
-          <Button negative onClick={onRemoveComparison}>
-            Remove Comparison
-          </Button>
-        </div>
-        <Grid columns={2} divided relaxed>
-          <Grid.Row>
-            <Grid.Column>
-              <Segment basic>
-                <Header size="huge" style={{ marginBottom: "1em" }}>
-                  <Header.Content>
-                    <CourseName
-                      uuid={course1Uuid}
-                      fallback={"(Unknown Name)"}
-                    />
-                    <Header.Subheader style={{ marginTop: "0.5em" }}>
-                      <CourseName
-                        uuid={course1Uuid}
-                        asSubjectAndNumber={true}
-                      />
-                    </Header.Subheader>
-                  </Header.Content>
-                </Header>
-                <Button
-                  primary
-                  icon="exchange"
-                  content="Replace Course"
-                  onClick={this.handleReplaceCourse1}
-                  style={{ marginBottom: "1em" }}
+  return (
+    <Container fluid style={{ padding: '20px 40px' }}>
+      <div style={{ display: 'flex', gap: '1em', marginBottom: '2em' }}>
+        <Button negative onClick={onRemoveComparison}>
+          Remove Comparison
+        </Button>
+      </div>
+      <Grid columns={2} divided relaxed>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment basic>
+              <Header size="huge" style={{ marginBottom: '1em' }}>
+                <Header.Content>
+                  <CourseName uuid={course1Uuid} fallback={'(Unknown Name)'} />
+                  <Header.Subheader style={{ marginTop: '0.5em' }}>
+                    <CourseName uuid={course1Uuid} asSubjectAndNumber={true} />
+                  </Header.Subheader>
+                </Header.Content>
+              </Header>
+              <Button
+                primary
+                icon="exchange"
+                content="Replace Course"
+                onClick={handleReplaceCourse1}
+                style={{ marginBottom: '1em' }}
+              />
+              <div className="chart-container" style={{ minHeight: '400px' }}>
+                <CourseChartViewer
+                  uuid={course1Uuid}
+                  instructorId={course1InstructorId}
+                  termCode={course1TermCode}
+                  onChange={handleCourse1Change}
                 />
-                <div className="chart-container" style={{ minHeight: "400px" }}>
-                  <CourseChartViewer
-                    uuid={course1Uuid}
-                    instructorId={course1InstructorId}
-                    termCode={course1TermCode}
-                    onChange={this.handleCourse1Change}
-                  />
-                </div>
-                <div
-                  className="chart-container"
-                  style={{ marginTop: "1em", minHeight: "300px" }}
-                >
-                  <CourseGpaChart uuid={course1Uuid} />
-                </div>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment basic>
-                <Header size="huge" style={{ marginBottom: "1em" }}>
-                  <Header.Content>
-                    <CourseName
-                      uuid={course2Uuid}
-                      fallback={"(Unknown Name)"}
-                    />
-                    <Header.Subheader style={{ marginTop: "0.5em" }}>
-                      <CourseName
-                        uuid={course2Uuid}
-                        asSubjectAndNumber={true}
-                      />
-                    </Header.Subheader>
-                  </Header.Content>
-                </Header>
-                <Button
-                  primary
-                  icon="exchange"
-                  content="Replace Course"
-                  onClick={this.handleReplaceCourse2}
-                  style={{ marginBottom: "1em" }}
+              </div>
+              <div className="chart-container" style={{ marginTop: '1em', minHeight: '300px' }}>
+                <CourseGpaChart uuid={course1Uuid} />
+              </div>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment basic>
+              <Header size="huge" style={{ marginBottom: '1em' }}>
+                <Header.Content>
+                  <CourseName uuid={course2Uuid} fallback={'(Unknown Name)'} />
+                  <Header.Subheader style={{ marginTop: '0.5em' }}>
+                    <CourseName uuid={course2Uuid} asSubjectAndNumber={true} />
+                  </Header.Subheader>
+                </Header.Content>
+              </Header>
+              <Button
+                primary
+                icon="exchange"
+                content="Replace Course"
+                onClick={handleReplaceCourse2}
+                style={{ marginBottom: '1em' }}
+              />
+              <div className="chart-container" style={{ minHeight: '400px' }}>
+                <CourseChartViewer
+                  uuid={course2Uuid}
+                  instructorId={course2InstructorId}
+                  termCode={course2TermCode}
+                  onChange={handleCourse2Change}
                 />
-                <div className="chart-container" style={{ minHeight: "400px" }}>
-                  <CourseChartViewer
-                    uuid={course2Uuid}
-                    instructorId={course2InstructorId}
-                    termCode={course2TermCode}
-                    onChange={this.handleCourse2Change}
-                  />
-                </div>
-                <div
-                  className="chart-container"
-                  style={{ marginTop: "1em", minHeight: "300px" }}
-                >
-                  <CourseGpaChart uuid={course2Uuid} />
-                </div>
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    );
-  }
+              </div>
+              <div className="chart-container" style={{ marginTop: '1em', minHeight: '300px' }}>
+                <CourseGpaChart uuid={course2Uuid} />
+              </div>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
 }
 
 export default CourseComparison;

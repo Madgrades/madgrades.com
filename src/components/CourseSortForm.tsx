05c37120aecabbdef19658d169411fc317faef4a
Keenan Thompson <keenan@keenant.com>
2025-11-22 22:58:32 +0000

    Convert CourseSortForm to functional component
    Errors: 756 → 752
    Class components: 10 → 9 remaining (15/24 complete, 63%)

--- src/components/CourseSortForm.tsx
+++ src/components/CourseSortForm.tsx
@@ -1,110 +1,104 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import utils from "../utils";
-import { Dropdown } from "semantic-ui-react";
-import { useNavigate } from "react-router-dom";
-import { stringify } from "qs";
+import { useState, useEffect } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import utils from '../utils';
+import { Dropdown } from 'semantic-ui-react';
+import { useNavigate } from 'react-router-dom';
+import { stringify } from 'qs';
+import { RootState, CourseFilterParams } from '../types';
 
 const sortOptions = [
   {
-    key: "relevance",
-    text: "Best Match",
-    value: "relevance",
+    key: 'relevance',
+    text: 'Best Match',
+    value: 'relevance',
   },
   {
-    key: "number",
-    text: "Number (Lowest First)",
-    value: "number",
+    key: 'number',
+    text: 'Number (Lowest First)',
+    value: 'number',
   },
   {
-    key: "number_desc",
-    text: "Number (Highest First)",
-    value: "number_desc",
+    key: 'number_desc',
+    text: 'Number (Highest First)',
+    value: 'number_desc',
   },
 ];
 
-class CourseFilterForm extends Component {
-  state = {
-    value: "number",
-  };
+interface OwnProps {
+  navigate: (path: string) => void;
+}
 
-  componentDidUpdate = (prevProps) => {
-    if (prevProps.courseFilterParams !== this.props.courseFilterParams) {
-      const { sort, order } = this.props.courseFilterParams;
-      let value;
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type Props = OwnProps & PropsFromRedux;
 
-      if (!sort) {
-        value = "relevance";
-      } else if (sort === "relevance") {
-        value = "relevance";
-      } else if (sort === "number") {
-        value = "number";
-        if (order === "desc") value = "number_desc";
-      }
+function CourseSortForm({ courseFilterParams, navigate }: Props) {
+  const [value, setValue] = useState('number');
 
-      if (value !== this.state.value) {
-        this.setState({
-          value,
-        });
-      }
+  useEffect(() => {
+    const { sort, order } = courseFilterParams;
+    let newValue;
+
+    if (!sort) {
+      newValue = 'relevance';
+    } else if (sort === 'relevance') {
+      newValue = 'relevance';
+    } else if (sort === 'number') {
+      newValue = 'number';
+      if (order === 'desc') newValue = 'number_desc';
     }
-  };
 
-  onChange = (event, { value }) => {
-    this.setState({
-      value,
-    });
+    if (newValue && newValue !== value) {
+      setValue(newValue);
+    }
+  }, [courseFilterParams, value]);
+
+  const handleChange = (event: any, { value: newValue }: { value: string }) => {
+    setValue(newValue);
 
     let sort, order;
 
-    if (value === "number") {
-      sort = "number";
-    } else if (value === "number_desc") {
-      sort = "number";
-      order = "desc";
-    } else if (value === "relevance") {
+    if (newValue === 'number') {
+      sort = 'number';
+    } else if (newValue === 'number_desc') {
+      sort = 'number';
+      order = 'desc';
+    } else if (newValue === 'relevance') {
       // nothing to do
     }
 
     const params = {
-      ...this.props.courseFilterParams,
+      ...courseFilterParams,
       sort,
       order,
     };
-    this.props.navigate("/search?" + stringify(params, { encode: false }));
+    navigate('/search?' + stringify(params, { encode: false }));
   };
 
-  render = () => {
-    const { value } = this.state;
-
-    return (
-      <Dropdown
-        inline
-        direction="left"
-        header="Sort options"
-        options={sortOptions}
-        value={value}
-        onChange={this.onChange}
-      />
-    );
-  };
+  return (
+    <Dropdown
+      inline
+      direction="left"
+      header="Sort options"
+      options={sortOptions}
+      value={value}
+      onChange={handleChange}
+    />
+  );
 }
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state: RootState) {
   return {
     courseFilterParams: state.app.courseFilterParams,
   };
 }
 
 // HOC to inject navigate as prop
-function withNavigate(Component) {
-  return function ComponentWithNavigate(props) {
+function withNavigate(Component: React.ComponentType<any>) {
+  return function ComponentWithNavigate(props: any) {
     const navigate = useNavigate();
     return <Component {...props} navigate={navigate} />;
   };
 }
 
-export default connect(
-  mapStateToProps,
-  utils.mapDispatchToProps
-)(withNavigate(CourseFilterForm));
+const connector = connect(mapStateToProps, utils.mapDispatchToProps);
+export default connector(withNavigate(CourseSortForm));
